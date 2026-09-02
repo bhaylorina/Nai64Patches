1c0ffeaa7468b663e184e1eb3e4e49f49987c4a8
bhaylorina <bhaylorina@gmail.com>
2026-09-02 22:39:07 +0530

    fix: Update ActiveManifestPatch to add FGS Permissions

--- patches/src/main/kotlin/patches/universal/manifest/ActiveManifestPatch.kt
+++ patches/src/main/kotlin/patches/universal/manifest/ActiveManifestPatch.kt
@@ -6,8 +6,8 @@ import java.util.logging.Logger
 @Suppress("unused")
 val activeManifestPatch = resourcePatch(
     name = "Add FGS Permissions",
-    description = "Injects FGS and God-Mode Provider",
-    default = false,
+    description = "Injects FGS Permissions",
+    default = true,
 ) {
     execute {
         var applied = false
@@ -15,7 +15,6 @@ val activeManifestPatch = resourcePatch(
             val root = m.documentElement ?: return@use
             val app = root.applicationOrNull() ?: return@use
 
-            // Permissions
             val p1 = m.createElement("uses-permission")
             p1.setAttributeNS(NS_ANDROID, "android:name", "android.permission.FOREGROUND_SERVICE")
             val p2 = m.createElement("uses-permission")
@@ -23,22 +22,14 @@ val activeManifestPatch = resourcePatch(
             root.appendChild(p1)
             root.appendChild(p2)
 
-            // Service
             val srv = m.createElement("service")
             srv.setAttributeNS(NS_ANDROID, "android:name", "app.morphe.patches.KeepAliveService")
             srv.setAttributeNS(NS_ANDROID, "android:exported", "false")
             srv.setAttributeNS(NS_ANDROID, "android:foregroundServiceType", "remoteMessaging")
             app.appendChild(srv)
 
-            // Auto-Init Content Provider (THE MAGIC TRICK)
-            val prv = m.createElement("provider")
-            prv.setAttributeNS(NS_ANDROID, "android:name", "app.morphe.patches.KeepAliveInitProvider")
-            prv.setAttributeNS(NS_ANDROID, "android:authorities", "com.twitter.android.morphe.keepalive")
-            prv.setAttributeNS(NS_ANDROID, "android:exported", "false")
-            app.appendChild(prv)
-
             applied = true
         }
-        if (applied) Logger.getLogger(this::class.java.name).info("Manifest Auto-Init Patched!")
+        if (applied) Logger.getLogger(this::class.java.name).info("Manifest FGS Patched!")
     }
 }
