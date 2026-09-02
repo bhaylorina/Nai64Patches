5d315844cdc54e2de49b1e2b4bd56b01f0b37c1c
bhaylorina <bhaylorina@gmail.com>
2026-09-03 02:06:27 +0530

    fix: Add extension configuration to build.gradle.kts

--- extensions/extension/build.gradle.kts
+++ extensions/extension/build.gradle.kts
@@ -5,3 +5,14 @@ extension {
 android {
     namespace = "io.github.nai64.extension"
 }
+extension {
+    name = "extensions/extension.mpe"
+}
+
+android {
+    namespace = "io.github.nai64.extension"
+}
+
+dependencies {
+    implementation("app.morphe:morphe-extensions-library:1.9.0")
+}
